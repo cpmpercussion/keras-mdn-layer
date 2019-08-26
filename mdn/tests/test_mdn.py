c85c1ea1879532bc12c5df1b079539753058f28c
Charles Martin <cpm@charlesmartin.com.au>
2019-08-27 00:04:57 +0200

    adjustments for mdn

--- mdn/tests/test_mdn.py
+++ mdn/tests/test_mdn.py
@@ -11,3 +11,15 @@ def test_build_mdn():
     model.add(mdn.MDN(1, N_MIXES))
     model.compile(loss=mdn.get_mixture_loss_func(1, N_MIXES), optimizer=keras.optimizers.Adam())
     assert isinstance(model, keras.engine.sequential.Sequential)
+
+
+def test_save_mdn():
+    N_HIDDEN = 5
+    N_MIXES = 5
+    model = keras.Sequential()
+    model.add(keras.layers.Dense(N_HIDDEN, batch_input_shape=(None, 1), activation='relu'))
+    model.add(mdn.MDN(1, N_MIXES))
+    model.compile(loss=mdn.get_mixture_loss_func(1, N_MIXES), optimizer=keras.optimizers.Adam())
+    model.save('test_save.h5')
+    m_2 = keras.models.load_model('test_save.h5', custom_objects={'MDN': mdn.MDN, 'mdn_loss_func': mdn.get_mixture_loss_func(1, N_MIXES)})
+    assert isinstance(m_2, keras.engine.sequential.Sequential)
